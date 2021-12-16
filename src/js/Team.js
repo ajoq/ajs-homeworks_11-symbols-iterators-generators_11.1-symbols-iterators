afab58b39249d036c2684e1a49a11032b74f6170
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-16 10:18:30 +0300

    done v2

--- src/js/Team.js
+++ src/js/Team.js
@@ -4,6 +4,21 @@ export default class Team {
   }
 
   [Symbol.iterator]() {
-    return this.players[Symbol.iterator]();
+    this.nextId = 0;
+    this.current = this.players[this.nextId];
+    return this;
   }
+
+  next() {
+    if (this.nextId < this.players.length) {
+      const result = { done: false, value: this.players[this.nextId++] };
+      return result;
+    }
+    return { done: true };
+  }
+
+  // Такой код тоже работает
+  //   [Symbol.iterator]() {
+  //     return this.players[Symbol.iterator]();
+  //   }
 }
